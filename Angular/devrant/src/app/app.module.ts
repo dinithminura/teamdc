ba9035c638e583156c051f32977ab9e9523bf09f
Chamith Dulanga <dulanga@vatichub.com>
2019-02-23 16:24:07 +0530

    Angular 6

--- Angular/devrant/src/app/app.module.ts
+++ Angular/devrant/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HellowWorldComponent } from './hellow-world/hellow-world.component';
 import { RantHeaderComponent } from './rant-header/rant-header.component';
 import { RantFooterComponent } from './rant-footer/rant-footer.component';
+import { RantListItemComponent } from './modules/home/rant-list-item/rant-list-item.component';
+import { RantDetailsPageComponent } from './modules/home/rant-details-page/rant-details-page.component';
+import { LoginPageComponent } from './modules/login/login-page/login-page.component';
+import { ArticalPageComponent } from './modules/home/artical-page/artical-page.component';
+import { PostCommentComponent } from './modules/home/post-comment/post-comment.component';
+import { AlertPopupComponent } from './modules/home/alert-popup/alert-popup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HellowWorldComponent,
     RantHeaderComponent,
-    RantFooterComponent
+    RantFooterComponent,
+    RantListItemComponent,
+    RantDetailsPageComponent,
+    LoginPageComponent,
+    ArticalPageComponent,
+    PostCommentComponent,
+    AlertPopupComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/devrant/src/app/modules/home/rant-details-page/rant-details-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rant-details-page',
+  templateUrl: './rant-details-page.component.html',
+  styleUrls: ['./rant-details-page.component.css']
+})
+export class RantDetailsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
