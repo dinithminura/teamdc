5de8585520ba7a2eff90655f1b499bdc61cea89c
Dinith Minura <dinithminura@gmail.com>
2019-02-23 13:47:51 +0530

    react-7

--- React/devrant/src/App.js
+++ React/devrant/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/header";
+import Spinner from "./components/spinner";
 
 class App extends Component {
   render() {
     return (
       <div class="page">
         <Header />
+        <Spinner/>
         {/* <!-- Start of Main Section --> */}
         {/* <!-- ======================= --> */}
 
